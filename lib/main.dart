ea0706a589410b98613ec0fb01c473ee5dc7d8b3
khalil <khalil.pan2@gmail.com>
2020-03-02 22:04:43 -0300

    adding Phone and Email

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -30,6 +31,55 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.0,
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 65.0, vertical: 10.0),
+                padding: EdgeInsets.all(2.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_phone,
+                      size: 40.0,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+55 81 00000-0000',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 65.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text('khalil.pan2@gmail.com'),
+                  ],
+                ),
+              )
             ],
           ),
         ),
